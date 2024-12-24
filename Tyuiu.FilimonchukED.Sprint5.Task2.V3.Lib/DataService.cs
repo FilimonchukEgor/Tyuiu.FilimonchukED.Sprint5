f9274b774dc4dc24e591c4e08c0d05858af9cd3a
Ернар Пупкин <gamerhey2@gmail.com>
2024-12-24 22:25:27 +0500

    Task2

---
+++ Tyuiu.FilimonchukED.Sprint5.Task2.V3.Lib/DataService.cs
@@ -0,0 +1,49 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+namespace Tyuiu.FilimonchukED.Sprint5.Task2.V3.Lib
+{
+    public class DataService : ISprint5Task2V3
+    {
+        public string SaveToFileTextData(int[,] matrix)
+        {
+            string tempDirectory = Path.GetTempPath();
+            string path = Path.Combine(tempDirectory, "OutPutFileTask2.csv");
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+
+            if (fileExists) File.Delete(path);
+
+            int rows = matrix.GetUpperBound(0) + 1;
+            int cols = matrix.Length / rows;
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    if (matrix[i, j] % 2 !=0) matrix[i, j] = 0;
+                }
+            }
+
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(path))
+                {
+                    // Проходим по строкам данных
+                    for (int i = 0; i < matrix.GetLength(0); i++)  // data.GetLength(0) - количество строк
+                    {
+                        string line = string.Join(";", // Используем точку с запятой как разделитель
+                            matrix[i, 0], matrix[i, 1], matrix[i, 2]); // Формируем строку с разделителями
+                        writer.WriteLine(line);  // Записываем строку в файл
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ошибка записи в файл: " + ex.Message);
+            }
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.FilimonchukED.Sprint5.Task2.V3/Program.cs
@@ -0,0 +1,50 @@
+﻿using Tyuiu.FilimonchukED.Sprint5.Task2.V3.Lib;
+namespace Tyuiu.FilimonchukED.Sprint5.Task2.V3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5 | Выполнил: Филимончук Е. Д. | РППб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #3                                                              *");
+            Console.WriteLine("* Выполнил: Филимончук Егор Дмитриевич | РППб-24-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("*          *");
+            Console.WriteLine("*                                          *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int[,] mtrx = new int[3, 3]
+            {
+                { 5, 9, 1 },
+                { 1, 3, 9 },
+                { 1, 2, 2 }
+            };
+            int rows = mtrx.GetUpperBound(0) + 1;
+            int cols = mtrx.Length / rows;
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++) 
+                {
+                    Console.Write($"{mtrx[i, j]} \t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            string res = ds.SaveToFileTextData(mtrx);
+
+            Console.WriteLine("Файл: " + res);
+            Console.WriteLine("Сохранен!");
+        }
+    }
+}
