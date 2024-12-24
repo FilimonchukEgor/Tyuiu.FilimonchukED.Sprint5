bc708783e9206843c3a2542186257880516aa53f
Ернар Пупкин <gamerhey2@gmail.com>
2024-12-24 20:44:22 +0500

    Task1

---
+++ Tyuiu.FilimonchukED.Sprint5.Task1.V15.Lib/DataService.cs
@@ -0,0 +1,54 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+using System.IO;
+namespace Tyuiu.FilimonchukED.Sprint5.Task1.V15.Lib
+{
+    public class DataService : ISprint5Task1V15
+    {
+        public string SaveToFileTextData(int startValue, int stopValue)
+        {
+            string tempDirectory = Path.GetTempPath();
+            string path = Path.Combine(tempDirectory, "OutPutFileTask1.txt");
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+
+            if (fileExists) File.Delete(path);
+
+            double y;
+            string strY;
+
+            var lines = new List<string>();
+
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                // Пытаемся избежать деления на 0 (x-0.4)
+                if (x == 0.4)
+                {
+                    lines.Add("0");
+                    continue;
+                }
+
+                y = Math.Round(Math.Cos(x) / (x - 0.4) + Math.Sin(x) * 8 * x + 2, 2);
+                strY = Convert.ToString(y);
+                lines.Add(strY);
+            }
+
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(path))
+                {
+                    foreach (var line in lines)
+                    {
+                        writer.WriteLine(line); 
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ошибка записи в файл" + ex.Message);
+            }
+
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.FilimonchukED.Sprint5.Task1.V15/Program.cs
@@ -0,0 +1,35 @@
+﻿using Tyuiu.FilimonchukED.Sprint5.Task1.V15.Lib;
+namespace Tyuiu.FilimonchukED.Sprint5.Task1.V15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5 | Выполнил: Филимончук Е. Д. | РППб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #15                                                             *");
+            Console.WriteLine("* Выполнил: Филимончук Егор Дмитриевич | РППб-24-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("*          *");
+            Console.WriteLine("*                                          *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int startValue = -5; int stopValue = 5;
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            string res = ds.SaveToFileTextData(startValue, stopValue);
+
+            Console.WriteLine("Файл: " + res);
+            Console.WriteLine("Cохранен!");
+        }
+    }
+}
