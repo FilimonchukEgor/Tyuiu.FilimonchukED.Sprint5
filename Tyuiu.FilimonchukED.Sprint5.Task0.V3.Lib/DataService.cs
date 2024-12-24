6f47da2b3d0ebc38c6ad85f141563243a3c904c3
Ернар Пупкин <gamerhey2@gmail.com>
2024-12-24 18:03:19 +0500

    Task0

--- Tyuiu.FilimonchukED.Sprint5.Task0.V3.Lib/DataService.cs
+++ Tyuiu.FilimonchukED.Sprint5.Task0.V3.Lib/DataService.cs
@@ -9,7 +9,7 @@ namespace Tyuiu.FilimonchukED.Sprint5.Task0.V3.Lib
             string tempDirectory = Path.GetTempPath();
             string path = Path.Combine(tempDirectory, "OutPutFileTask0.txt");
 
-            double y = -0.25 * (Math.Pow(x, 3) - (3 * Math.Pow(x, 2) + 4));
+            double y = -0.25 * (Math.Pow(x, 3) - 3 * Math.Pow(x, 2) + 4);
             y = Math.Round(y, 3);
 
             try
