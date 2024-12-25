1af84fbad91c504c1f4e62b0580f57de3781d2c4
Ернар Пупкин <gamerhey2@gmail.com>
2024-12-25 19:05:39 +0500

    Task3

--- Tyuiu.FilimonchukED.Sprint5.Task3.V14.Lib/DataService.cs
+++ Tyuiu.FilimonchukED.Sprint5.Task3.V14.Lib/DataService.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO;
+using System.Text;
 using tyuiu.cources.programming.interfaces.Sprint5;
 namespace Tyuiu.FilimonchukED.Sprint5.Task3.V14.Lib
 {
@@ -8,12 +9,18 @@ namespace Tyuiu.FilimonchukED.Sprint5.Task3.V14.Lib
         {
             string tempDirectory = Path.GetTempPath();
             string path = Path.Combine(tempDirectory, "OutPutFileTask3.bin");
-            
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+
+            if (fileExists) File.Delete(path);
+
             double y;
             y = 4 * Math.Pow(x, 3) / (Math.Pow(x, 3) - 1);
             y = Math.Round(y, 3);
 
-            using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.OpenOrCreate)))
+            using (FileStream fs = new FileStream(path, FileMode.Create, FileAccess.Write))
+            using (StreamWriter writer = new StreamWriter(fs, Encoding.UTF8))
             {
                 writer.Write(BitConverter.GetBytes(y));
             }
