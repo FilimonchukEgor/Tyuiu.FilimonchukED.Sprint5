d7f4dd41ea9bb0db7ebfa871075cfd60b902ce74
Ернар Пупкин <gamerhey2@gmail.com>
2024-12-25 18:48:36 +0500

    Task3

---
+++ Tyuiu.FilimonchukED.Sprint5.Task3.V14.Lib/DataService.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.FilimonchukED.Sprint5.Task3.V14.Lib
+{
+    public class DataService : ISprint5Task3V14
+    {
+        public string SaveToFileTextData(int x)
+        {
+            string tempDirectory = Path.GetTempPath();
+            string path = Path.Combine(tempDirectory, "OutPutFileTask3.bin");
+            
+            double y;
+            y = 4 * Math.Pow(x, 3) / (Math.Pow(x, 3) - 1);
+            y = Math.Round(y, 3);
+
+            using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.OpenOrCreate)))
+            {
+                writer.Write(BitConverter.GetBytes(y));
+            }
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.FilimonchukED.Sprint5.Task3.V14/Program.cs
@@ -0,0 +1,35 @@
+﻿using Tyuiu.FilimonchukED.Sprint5.Task3.V14.Lib;
+namespace Tyuiu.FilimonchukED.Sprint5.Task3.V14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5 | Выполнил: Филимончук Е. Д. | РППб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #22                                                             *");
+            Console.WriteLine("* Выполнил: Филимончук Егор Дмитриевич | РППб-24-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("*          *");
+            Console.WriteLine("*                                          *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int x = 3;
+            Console.WriteLine("X = "+x);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            string str = ds.SaveToFileTextData(x);
+            Console.WriteLine("Файл: "+str);
+            Console.WriteLine("Создан!");
+        }
+    }
+}
