6a21976456cbdf07d035b03d790f75f6838d78d0
Ернар Пупкин <gamerhey2@gmail.com>
2024-12-28 06:00:50 +0500

    Task5

---
+++ Tyuiu.FilimonchukED.Sprint5.Task5.V6.Lib/DataService.cs
@@ -0,0 +1,21 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.FilimonchukED.Sprint5.Task5.V6.Lib
+{
+    public class DataService : ISprint5Task5V6
+    {
+        public double LoadFromDataFile(string path)
+        {
+            path = Path.GetTempFileName();
+            double res = 0;
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null) 
+                {
+                    res = (res + Convert.ToDouble(line))/line.Length;
+                }
+            }
+            return res;
+        }
+    }
+}
