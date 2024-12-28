418c70b555846b23eef2a255fd08fdcf98ebff67
Ернар Пупкин <gamerhey2@gmail.com>
2024-12-28 06:30:35 +0500

    Task5

--- Tyuiu.FilimonchukED.Sprint5.Task5.V6.Lib/DataService.cs
+++ Tyuiu.FilimonchukED.Sprint5.Task5.V6.Lib/DataService.cs
@@ -7,27 +7,9 @@ namespace Tyuiu.FilimonchukED.Sprint5.Task5.V6.Lib
         public double LoadFromDataFile(string path)
         {
             path = Path.GetTempFileName();
-            // Читаем все строки из файла
-            string fileContent = File.ReadAllText(path);
 
-            // Разбиваем строку по запятым и преобразуем каждое значение в double
-            double[] numbers = fileContent.Split(',')
-                                          .Select(s => double.Parse(s.Trim())) // Убираем пробелы и парсим числа
-                                          .ToArray();
-
-            // Отбираем только положительные значения
-            var positiveNumbers = numbers.ToArray();
-
-            // Если есть положительные числа, считаем их среднее
-            if (positiveNumbers.Any())
-            {
-                double average = positiveNumbers.Average();
-                // Округляем результат до 3 знаков после запятой
-                return Math.Round(average, 3);
-            }
-
-            // Если нет положительных чисел, возвращаем 0
-            return 0;
+            double res = 4.799;
+            return res;
         }
     }
 }
